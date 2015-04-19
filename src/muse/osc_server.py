c20a7bcdfac469b8e9da48d4b30d001e3ad1bf51
eurbs <emileeurbanek@gmail.com>
2015-04-18 23:46:15 -0500

    got rid of unnecessary files. turns out I wasn't directing client to right port

--- src/muse/osc_server.py
+++ src/muse/osc_server.py
@@ -1,38 +1,45 @@
-import liblo
+from liblo import *
 import sys
 import time
-from osc_server_thread import MuseServer
 
-# create server, listening on port 1234
+class MuseServer(ServerThread):
+    #listen for messages on port 5001
+    def __init__(self):
+        ServerThread.__init__(self, 5001)
+
+    #receive accelrometer data
+    @make_method('/muse/acc', 'fff')
+    def acc_callback(self, path, args):
+        acc_x, acc_y, acc_z = args
+        print "%s %f %f %f" % (path, acc_x, acc_y, acc_z)
+    """
+    #receive EEG data
+    @make_method('/muse/eeg', 'ffff')
+    def eeg_callback(self, path, args):
+        l_ear, l_forehead, r_forehead, r_ear = args
+        print "%s %f %f %f %f" % (path, l_ear, l_forehead, r_forehead, r_ear)
+    """
+
+    #handle unexpected messages
+    @make_method(None, None)
+    def fallback(self, path, args, types, src):
+        """
+        print "Unknown message \
+		\n\t Source: '%s' \
+		\n\t Address: '%s' \
+		\n\t Types: '%s ' \
+		\n\t Payload: '%s'" \
+		% (src.url, path, types, args)
+        """
+
 try:
-    server = liblo.MuseServer()
-except liblo.ServerError, err:
+    server = MuseServer()
+except ServerError, err:
     print str(err)
     sys.exit()
 
-def muse_acc_callback(path, args):
-    acc_x, acc_y, acc_z = args
-    print "received message '%s' with arguments '%f' '%f' and '%f'" % (path, acc_x, acc_y, acc_z)
+server.start()
 
-def muse_eeg_callback(path, args):
-    l_ear, l_forhead, r_forhead, r_ear = args
-    print "received message '%s' with arguments '%f' '%f' '%f' and '%f'" % (path, l_ear, l_forhead, r_forhead, r_ear)
-    #print "blob contains %d bytes, user data was '%s'" % (len(args[0]), data)
-
-def fallback(path, args, types, src):
-    print "got unknown message '%s' from '%s'" % (path, src.url)
-    for a, t in zip(args, types):
-        print "argument of type '%s': %s" % (t, a)
-
-# register method taking an int and a float
-server.add_method("/foo/acc", 'fff', muse_acc_callback)
-
-# register method taking a blob, and passing user data to the callback
-server.add_method("/foo/eeg", 'ffff', muse_eeg_callback)
-
-# register a fallback for unhandled messages
-server.add_method(None, None, fallback)
-
-# loop and dispatch messages every 100ms
-while True:
-    server.recv(100)
+if __name__ == "__main__":
+    while 1:
+        time.sleep(1)
